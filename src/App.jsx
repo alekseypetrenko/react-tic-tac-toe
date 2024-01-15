c64926e6934699dbd2a95ce88667c9ea6c2de434
alekseypetrenko <menefrego5@gmail.com>
2024-01-15 16:33:11 +0200

    added player

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name={"as"} symbol={"x"} />
+          <Player name={"as dsc "} symbol={"o"} />
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
