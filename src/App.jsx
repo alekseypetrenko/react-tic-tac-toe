8eb0984f62636f90276cf41785a4ef547381a641
alekseypetrenko <menefrego5@gmail.com>
2024-01-17 01:25:45 +0200

    added game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
           <Player initialName={"as"} symbol={"x"} />
           <Player initialName={"as dsc "} symbol={"o"} />
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
